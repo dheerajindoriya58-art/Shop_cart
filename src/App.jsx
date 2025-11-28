a18560aaae09251163fc544a3a944bbf788a24ee
Dheeraj Indoriya <dheerajindoriya58@gmail.in>
2025-11-28 21:41:20 +0530

    New files

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,29 @@
 import React from "react";
-import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import AppLayout from "./AppLayout";
 import Home from "./pages/Home";
-import { ProductProvider } from "./context/ContextProvider";
-import "./App.css";
-import "./shop.css";
-import Product from "./pages/Product";
-import Cart from "./pages/Cart";
-import Detail from "./pages/Detail";
-import Contact from "./pages/Contact";
-import Hero from "./pages/Hero";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import CartPage from "./pages/CartPage";
+import { CartProvider } from "./context/CartContext";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/products", element: <Products /> },
+      { path: "/product/:id", element: <ProductDetail /> },
+      { path: "/cart", element: <CartPage /> },
+    ],
+  },
+]);
 
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />
-  //   },
-  //   {
-  //     path: "/cart",
-  //     element: <Cart/>
-  //   },
-  //   {
-  //     path: "/count",
-  //     element: <Count />
-  //   }
-  // ]);
-
-  // return (
-  //     <RouterProvider router={router} />
-  // );
-
-  return (
-    <ProductProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hero" element={<Hero />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/product/:id" element={<Detail />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-    </ProductProvider>
-  )
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CartProvider>
+    <RouterProvider router={router} />
+  </CartProvider>
+);
