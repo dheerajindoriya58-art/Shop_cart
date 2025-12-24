4cf73d7c66622a072305b1f54ee2838f4fc1e95f
Dheeraj Indoriya <dheerajindoriya58@gmail.in>
2025-12-24 15:34:42 +0530

    Intital files

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,7 +4,7 @@ export default function CartPage() {
   const { cart, add, removeOne, deleteItem } = useCart();
 
   return (
-    <div className="max-w-6xl mx-auto p-4">
+    <div className="max-w-[1070px] mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6">Your Cart</h1>
 
       {cart.length === 0 && (
@@ -52,7 +52,7 @@ export default function CartPage() {
             {/* Delete Button */}
             <button
               onClick={() => deleteItem(item.id)}
-              className="bg-[#d41919] hover:bg-[#eb3434] text-white mt-2 p-[8px_24px] rounded md:mt-0"
+              className="bg-[#d41919] hover:border-2 hover:border-green-700 duration-300 text-white mt-2 p-[8px_24px] rounded md:mt-0 hover:bg-transparent hover:text-black cursor-pointer"
             >
               Delete
             </button>
